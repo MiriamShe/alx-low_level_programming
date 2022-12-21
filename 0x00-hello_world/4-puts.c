ed4fcfd6dfcb99b5251821f8b614952a56dd5d85
MiriamShe <miriamwanjiru019@gmail.com>
2022-12-21 06:32:45 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * return - always 0 (Success)
+ */
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	retun (0);
+}
