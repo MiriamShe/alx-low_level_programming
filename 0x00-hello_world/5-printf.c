f1306c59f3d866fabace079f275fb84080858c65
MiriamShe <miriamwanjiru019@gmail.com>
2022-12-21 06:49:06 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+/**
+ * Description: main - prints out a string to stdout.
+ * Return: 0 if success
+ */
+int main(void)
+{
+	printf("With proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
