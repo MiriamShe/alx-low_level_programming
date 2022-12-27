7760cdfd533b8658d527876bac7d4282c16ae62d
MiriamShe <miriamwanjiru019@gmail.com>
2022-12-27 06:59:45 -0800

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * main - prints _putchar, fllowd by a new linees
- * Return: Alwys 0 (Success)
+ * main - prints _putchar, followed by a new line
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 	char abe[] = "_putchar";
-
 	int c;
 
 	for (c = 0; c < 8; c++)
