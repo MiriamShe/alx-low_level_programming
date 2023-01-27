46cf76eecd54ebd516ce58d3481d25e6e8b5b8f1
MiriamShe <miriamwanjiru019@gmail.com>
2023-01-26 17:57:08 -0800

    A dream doesn't become reality through magic; it takes sweat, determination and hard work

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,12 +6,13 @@
  * int prototypes()
  * char prototypes()
  */
-void reverse_array(int *a, int n);
+void reverse_array(int *a, int n;
 void print_number(int n);
 void print_buffer(char *b, int size);
 int _strcmp(char *s1, char *s2);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
